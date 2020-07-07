5eb12cb2a5955c48da07652d4234ced65a90d802
Juwan <junhuanchen@qq.com>
2020-07-07 10:17:48 +0800

    updated ui_launcher

--- ui_launcher.py
+++ ui_launcher.py
@@ -1,64 +1,27 @@
-import image
-import lcd
 import math
-import gc
+from maix_ui import ui
+from ui_taskbar import taskbar
 
-gc.collect()
-
-lcd.init(freq=15000000)
-
-def print_mem_free():
-    print('ram total : ' + str(gc.mem_free() / 1024) + ' kb')
-
-class ui:
-
-    alpha, img, logo, bak = 0, None, None, None
-
-    def warp_template(func):
-        def tmp_warp(warp=None):
-            if warp:
-              return lambda *args: [func(), warp()]
-        return tmp_warp
-
-    def bg_draw():
-        gc.collect()
-        if ui.bak == None:
-            ui.bak = image.Image("/sd/res/images/bg.jpg") # 90ms
-        ui.img = ui.bak.copy() # 10ms
-
-    def logo_draw():
-        ui.bg_draw()
-        value = math.cos(math.pi * ui.alpha / 24) * 60 + 100
-        ui.alpha = (ui.alpha + 1) % 48
-        if ui.logo == None:
-            ui.logo = image.Image("/sd/res/images/logo.jpg") # 90ms
-        tmp = ui.logo.copy() # 10ms
-        ui.img.draw_image(tmp, 50, 50, alpha=int(value)) # 50ms
-        del tmp
-
-    def display():
-        lcd.display(ui.img)
-        del ui.img
-
-if __name__ == "__main__":
+class launcher:
     alpha = 0
     app_settings = image.Image("/sd/res/icons/app_settings.bmp") # 60ms
     app_explorer = image.Image("/sd/res/icons/app_explorer.bmp") # 60ms
     app_system_info = image.Image("/sd/res/icons/app_system_info.bmp") # 60ms
     app_camera = image.Image("/sd/res/icons/app_camera.bmp") # 60ms
+
     @ui.warp_template(ui.logo_draw)
-    def test_launcher_draw():
+    @ui.warp_template(taskbar.time_draw)
+    def event():
          #print_mem_free()
         #lcd.display(ui.img) # 15ms
         #return
         ## print_mem_free()
-        global alpha
-        value = math.cos(math.pi * alpha / 12) * 100 + 100
-        alpha = (alpha + 1) % 24
+        value = math.cos(math.pi * launcher.alpha / 12) * 90 + 160
+        launcher.alpha = (launcher.alpha + 1) % 24
         #value = 255
 
         #tmp = image.Image("/sd/res/icons/app_camera.bmp") # 60ms
-        tmp = app_camera.copy() # 1ms
+        tmp = launcher.app_camera.copy() # 1ms
         ui.img.draw_image(tmp, 40, 40, alpha=int(value)) # 4ms
         del tmp
 
@@ -69,7 +32,7 @@ if __name__ == "__main__":
             b'\x10\x11\x11\x11\xFD\x11\x31\x39\x55\x55\x91\x11\x11\x12\x12\x14\x00\xF0\x10\x10\x10\x10\x10\x10\x10\x10\x10\x12\x12\x12\x0E\x00'
             , scale=1, color=(0,255,0))
 
-        tmp = app_system_info.copy() # 1ms
+        tmp = launcher.app_system_info.copy() # 1ms
         #tmp = image.Image("/sd/res/icons/app_system_info.bmp") # 60ms
         ui.img.draw_image(tmp, 140, 40, alpha=int(value)) # 4ms
         del tmp
@@ -81,7 +44,7 @@ if __name__ == "__main__":
             b'\x01\x02\x1F\x10\x1F\x10\x1F\x10\x1F\x10\x01\x08\x48\x48\x87\x00\x00\x00\xF0\x10\xF0\x10\xF0\x10\xF0\x10\x00\x84\x92\x12\xF0\x00'
             , scale=1, color=(0,255,0))
 
-        tmp = app_explorer.copy() # 1ms
+        tmp = launcher.app_explorer.copy() # 1ms
         #tmp = image.Image("/sd/res/icons/app_explorer.bmp") # 60ms
         ui.img.draw_image(tmp, 140, 140, alpha=int(value)) # 4ms
         del tmp
@@ -93,7 +56,7 @@ if __name__ == "__main__":
             b'\x00\x27\x14\x14\x85\x45\x45\x15\x15\x25\xE4\x24\x29\x2A\x30\x00\x00\xFE\x20\x40\xFC\x04\xFC\x04\xFC\x24\x20\xA8\x24\x22\xA0\x40'
             , scale=1, color=(0,255,0))
 
-        tmp = app_settings.copy() # 1ms
+        tmp = launcher.app_settings.copy() # 1ms
         #tmp = image.Image("/sd/res/icons/app_settings.bmp") # 60ms
         ui.img.draw_image(tmp, 40, 140, alpha=int(value)) # 4ms
         del tmp
@@ -109,16 +72,9 @@ if __name__ == "__main__":
 
         ui.display()
 
-        print_mem_free()
-
+if __name__ == "__main__":
 
     import time
-    last = time.ticks_ms()
     while True:
-        try:
-            print(time.ticks_ms() - last)
-            last = time.ticks_ms()
-            test_launcher_draw()
-        except Exception as e:
-            gc.collect()
-            print(e)
+        launcher.event()
+
