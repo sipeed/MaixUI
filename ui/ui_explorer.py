ca0fb24da3cec29fe48ca2c26e7050b63c30d188
Juwan <junhuanchen@qq.com>
2020-07-15 15:36:04 +0800

    Provide UI explorer.

--- ui/ui_explorer.py
+++ ui/ui_explorer.py
@@ -13,90 +13,107 @@ import os, machine, ubinascii
 
 from fs import OS
 
-print(dir(OS))
 
-class ui_explorer:
+def list_rshift(obj):
+    obj.insert(0, obj[-1])
+    obj.pop(-1)
+    return obj
+
+#print(list_rshift([0, 1, 2]))
+
+def list_lshift(obj):
+    obj.append(obj.pop(0))
+    return obj
+
+#print(list_lshift([0, 1, 2]))
+
+class explorer:
 
   btn = cube_button()
-  files = OS.listdir('/')
-  path = '/'
-  index = 0
-  layer = ['../']
-  limit = 2
+  files = OS.listdir('/') + ["/"]
+  paths = ["/"]
+  info = ""
 
-  def view_ui_explorer(path='/',selected=0):
-    x_offset, y_offset = 10, 25
+  def load_explorer(path='/'):
+    if '../' == path: # return
+        #print(2, explorer.paths, explorer.files)
+        explorer.paths.pop(-1)
+        if len(explorer.paths) > 1:
+            explorer.files = OS.listdir(explorer.get_path(explorer.paths[-1]))
+            explorer.files.insert(0, '../')
+        else:
+            explorer.files = OS.listdir('/')
+            explorer.files.insert(0, '/')
+        #print(3, explorer.paths, explorer.files)
+        return # !
 
-    ui.img.draw_string(x_offset, y_offset, ui_explorer.path)
+    if explorer.paths[-1] != path: # >>>
+        if path[0] != '/':
+          path = '/' + path
+        explorer.paths.append(path)
+        #print(0, explorer.get_path(explorer.paths), OS.listdir(explorer.get_path(explorer.paths)))
+        explorer.files = OS.listdir(explorer.get_path(explorer.paths))
+        explorer.files.insert(0, '../')
+        #print(1, explorer.paths, explorer.files)
+        return # !
 
-    files = OS.listdir('/')
-
-    y_offset += 20
-
-    ui.img.draw_string(x_offset, y_offset, str(files))
-
-
+  def get_path(paths=[]):
+      tmp_path = ''
+      for p in explorer.paths:
+          tmp_path += p if p != '/' else ''
+      return tmp_path
 
   def draw():
-    ui_explorer.btn.event()
+    explorer.btn.event()
 
-    if ui_explorer.btn.back() == 1:
-        ui_explorer.index -= 1
+    if explorer.btn.back() == 1:
+        list_rshift(explorer.files)
+    elif explorer.btn.next() == 1:
+        list_lshift(explorer.files)
+    elif explorer.btn.home() == 1:
+        tmp = explorer.files[0]
+        if tmp == '/':
+            raise Exception('exit explorer...')
+        elif tmp in ["../"] or tmp.find('.') == -1:
+            explorer.load_explorer(tmp)
+            list_lshift(explorer.files)
+        else:
+            explorer.info = tmp
 
-    elif ui_explorer.btn.next() == 1:
-        ui_explorer.index += 1
+    ui.img.draw_rectangle((0,0,240,240), fill=True, color=(50, 50, 50))
 
-    x_offset, y_offset = 10, 25
+    ui.img.draw_string(10, 5, str(explorer.get_path(explorer.paths)), scale=1)
 
-    path = '/'
+    ui.img.draw_string(10, 225, str(explorer.info), scale=1)
 
-    ui_explorer.files = OS.listdir(path)
+    list_size = len(explorer.files)
 
-    ui_explorer.files.append(path)
+    if list_size > 4:
+        tmp = (150, 150, 150)
+        ui.img.draw_rectangle((65, 25, 200, 25), fill=True, color=(75, 75, 75))
+        ui.img.draw_string(70, 30, str(explorer.files[-2]), scale=1, color=tmp)
+        ui.img.draw_rectangle((65, 195, 200, 25), fill=True, color=(75, 75, 75))
+        ui.img.draw_string(70, 200, str(explorer.files[+2]), scale=1, color=tmp)
 
-    for i in range(0, ui_explorer.index):
-        ui_explorer.files.append(ui_explorer.files.pop(0))
+    if list_size > 2:
+        tmp = (200, 200, 200)
+        ui.img.draw_rectangle((45, 55, 200, 35), fill=True, color=(100, 100, 100))
+        ui.img.draw_string(50, 60, str(explorer.files[-1]), scale=2, color=tmp)
+        ui.img.draw_rectangle((45, 155, 200, 35), fill=True, color=(100, 100, 100))
+        ui.img.draw_string(50, 160, str(explorer.files[+1]), scale=2, color=tmp)
 
-    ui.img.draw_string(x_offset, y_offset, str(ui_explorer.files))
-
-    ui_explorer.index = ui_explorer.index % len(ui_explorer.files)
-
-
-    #ui_explorer.view_ui_explorer()
-
-    # y_offset += 18
-    # for i in range(self.current_offset, len(self.current_dir_files)):
-    #     gc.collect()
-    #     file_name = self.current_dir_files[i]
-    #     # print(file_name)
-    #     try:
-    #         f_stat = os.stat(self.path + file_name)
-    #         if len(file_name) > 16:
-    #             file_name = file_name[0:14] + ".."
-    #         if S_ISDIR(f_stat[0]):
-    #             file_name = file_name + '/'
-    #         gc.collect()
-    #         file_readable_size = sizeof_fmt(f_stat[6])
-    #         img.draw_string(lcd.width() - 50, y_offset,
-    #                         file_readable_size, lcd.BLUE)
-    #     except Exception as e:
-    #         print("-------------------->", e)
-    #     is_current = self.current_selected_index == i
-    #     line = "%s %d %s" % ("->" if is_current else "  ", i, file_name)
-    #     img.draw_string(x_offset, y_offset, line, lcd.RED)
-    #     # gc.collect()
-    #     y_offset += 18
-    #     if y_offset > lcd.height():
-    #         print(y_offset, lcd.height(), "y_offset > height(), break")
-    #         break
-
+    if list_size > 0:
+        tmp = (255, 255, 255)
+        #ui.img.draw_ellipse(120, 120, 200, 25, 0, color=(226, 205, 223), thickness=2, fill=True)
+        ui.img.draw_rectangle((20, 95, 220, 55), fill=True, color=(0x5b, 0x86, 0xec))
+        ui.img.draw_string(30, 105, str(explorer.files[0]), scale=3, color=tmp)
 
 if __name__ == "__main__":
 
     @ui.warp_template(ui.blank_draw)
-    @ui.warp_template(ui.bg_in_draw)
+    #@ui.warp_template(ui.bg_in_draw)
     #@ui.warp_template(ui.anime_in_draw)
-    @ui.warp_template(ui_explorer.draw)
+    @ui.warp_template(explorer.draw)
     def unit_test():
       ui.display()
 
