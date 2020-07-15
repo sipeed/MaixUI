85a29091673670bebd8304d397cbb17d1e163506
Juwan <junhuanchen@qq.com>
2020-07-15 15:35:37 +0800

    taskbar.py add battery_draw

--- ui/ui_taskbar.py
+++ ui/ui_taskbar.py
@@ -13,31 +13,40 @@ from ui_maix import ui
 
 from pmu_axp173 import AXP173
 
+levels = [4.13, 4.06, 3.98, 3.92, 3.87, 3.82, 3.79, 3.77, 3.74, 3.68, 3.45, 3.00]
 
 class taskbar:
 
-    info, last = '', 0
+    now, info, last, img = '', '', 0, None
 
     axp173 = AXP173()
 
     def time_draw():
-        now = 45678 + time.ticks_ms() / 1000
-        taskbar.info = time.localtime(int(now))
-        ui.img.draw_string(60, 2, "%02u:%02u:%02u" % (
-            taskbar.info[3], taskbar.info[4], taskbar.info[5]), scale=2, mono_space=1)
+        now = 0 + time.ticks_ms() / 1000
+        taskbar.now = time.localtime(int(now))
+        ui.img.draw_string(30, 100, "%02u:%02u:%02u" % (
+            taskbar.now[3], taskbar.now[4], taskbar.now[5]), color=(255, 255, 255), scale=3, mono_space=1)
 
     def mem_draw():
         info = 'Rmnng %s KB' % str(gc.mem_free() / 1024)
         ui.img.draw_string(10, 2, info, scale=2)
 
     def battery_draw():
-        #img = image.Image("res/icons/battery/0.jpg")
-        #ui.img.draw_image(img, 10, 2, alpha=int(120))
         if taskbar.last + 3000 < time.ticks_ms():
             taskbar.last = time.ticks_ms()
-            vbat_voltage = taskbar.axp173.getVbatVoltage()
-            taskbar.info = "{0}".format(vbat_voltage, taskbar.axp173.is_charging())
-        ui.img.draw_string(10, 2, taskbar.info, scale=2)
+            vbat_voltage = taskbar.axp173.getVbatVoltage() / 1000
+            taskbar.info = "{0} V".format(vbat_voltage, taskbar.axp173.is_charging())
+            if taskbar.img != None:
+                del taskbar.img
+            pos = int((4.2 - vbat_voltage) / 0.2)
+            if pos < 6:
+                tmp = "charge" if taskbar.axp173.is_charging() else "normal"
+                taskbar.img = image.Image("res/icons/battery/{0} ({1}).jpg".format(tmp, 6 - pos))
+
+        if taskbar.img != None:
+            #ui.img.draw_rectangle((0,0,240,25), fill=True, color=(50, 50, 50))
+            ui.img.draw_image(taskbar.img, 10, 2, alpha=int(255))
+        ui.img.draw_string(100, 2, taskbar.info, scale=2, color=(255, 50, 50))
 
 # config usb input limit 190ma
 taskbar.axp173.enable_adc(True)
@@ -47,12 +56,23 @@ taskbar.axp173.exten_output_enable()
 
 if __name__ == "__main__":
 
+    def tmp_draw():
+        ui.img.draw_rectangle((0,0,240,240), fill=True, color=(75, 75, 75))
+
     @ui.warp_template(ui.blank_draw)
-    @ui.warp_template(ui.bg_in_draw)
+    @ui.warp_template(tmp_draw)
+    @ui.warp_template(ui.anime_in_draw)
+    @ui.warp_template(taskbar.time_draw)
     @ui.warp_template(taskbar.battery_draw)
     def app_main():
         ui.display()
     import time
+    last = time.ticks_ms()
     while True:
-        app_main()
-        #time.sleep(0.5)
+        try:
+            print(time.ticks_ms() - last)
+            last = time.ticks_ms()
+            app_main()
+        except Exception as e:
+            gc.collect()
+            print(e)
