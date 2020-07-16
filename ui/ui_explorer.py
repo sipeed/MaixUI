0f87dc5b005698eb2362f920c97fb3a018898193
junhuanchen <junhuanchen@qq.com>
2020-07-17 00:30:10 +0800

    test.

--- ui/ui_explorer.py
+++ ui/ui_explorer.py
@@ -91,8 +91,9 @@ class explorer:
             explorer.info = tmp
             protect.stop()
             # with open(explorer.get_path(explorer.paths) + '/' + tmp, 'rb') as target:
-            #     exec(target.read())
-            execfile(explorer.get_path(explorer.paths) + '/' + tmp, globals(), locals())
+            #     exec(target.read(), globals())
+            print(explorer.get_path(explorer.paths) + '/' + tmp)
+            execfile(explorer.get_path(explorer.paths) + '/' + tmp, globals())
             protect.start()
 
     ui.canvas.draw_rectangle((0, 0, 240, 240), fill=True, color=(50, 50, 50))
