64318cc29e3037d27d0afd2ab5908f34a15d5bad
junhuanchen <junhuanchen@qq.com>
2020-07-31 23:45:03 +0800

    fix led config.

--- driver/led.py
+++ driver/led.py
@@ -4,16 +4,19 @@ from Maix import FPIOA, GPIO
 
 class cube_led:
 
-    r = GPIO(GPIO.GPIOHS13, GPIO.OUT)
-    g = GPIO(GPIO.GPIOHS12, GPIO.OUT)
-    b = GPIO(GPIO.GPIOHS14, GPIO.OUT)
-    w = GPIO(GPIO.GPIOHS3, GPIO.OUT)
+    r, g, b, w = None, None, None, None
 
     def init(r = 13, g = 12, b = 14, w = 32):
         fm.register(r, fm.fpioa.GPIOHS13)
         fm.register(g, fm.fpioa.GPIOHS12)
         fm.register(b, fm.fpioa.GPIOHS14)
         fm.register(w, fm.fpioa.GPIOHS3)
+
+        cube_led.r = GPIO(GPIO.GPIOHS13, GPIO.OUT)
+        cube_led.g = GPIO(GPIO.GPIOHS12, GPIO.OUT)
+        cube_led.b = GPIO(GPIO.GPIOHS14, GPIO.OUT)
+        cube_led.w = GPIO(GPIO.GPIOHS3, GPIO.OUT)
+
         cube_led.r.value(1)
         cube_led.g.value(1)
         cube_led.b.value(1)
