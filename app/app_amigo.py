3a531075bb31737ee9a5ed09d7c598f555319339
Juwan <junhuanchen@qq.com>
2020-08-21 19:14:09 +0800

    add amigo launcher.

--- app/app_amigo.py
+++ app/app_amigo.py
@@ -5,210 +5,122 @@
 #   http://www.opensource.org/licenses/mit-license.php
 #
 
-import time, gc
+import math, os, image
 
-from core import agent
-from ui_canvas import ui, print_mem_free
-# from ui_launcher import launcher
-from ui_3d_launcher import launcher
-from ui_system_info import system_info
-from ui_catch import catch
-from ui_pages import pages
-from ui_camera import ai_camera
-from ui_sample import sample_page
-from ui_explorer import explorer
-from sample_shtxx import sample_shtxx
-from sample_spmod import sample_spmod_test
-from sample_msa301 import sample_msa301
-from button import sipeed_button, button_io
-from wdt import protect
-from led import cube_led
-from sound import CubeAudio
-#from ui_taskbar import taskbar
+try:
+    from ui_canvas import ui
+    from button import sipeed_button, button_io
+    from core import agent
+except ImportError:
+    from ui.ui_canvas import ui
+    from driver.button import sipeed_button, button_io
+    from lib.core import agent
 
-class app:
+class icon:
 
-    layer = 0 # set help_draw to top
-    ctrl = agent()
-    btn = sipeed_button()
+  def __init__(self, x, y, path):
+    self.img = image.Image(path)  # 60ms ~ 90ms
+    self.x, self.y = x, y
+    self.w, self.h = self.img.width(), self.img.height()
+    self.last = 0
 
-    @ui.warp_template(ui.bg_in_draw)
-    @ui.warp_template(ui.help_in_draw)
-    def draw_load():
-        ui.display()
+  def checked(self, color=(255, 0, 0)):
+    ui.canvas.draw_rectangle(self.x - 2, self.y - 2,
+                             self.w + 4, self.h + 4, color, thickness=1)
 
-    # @ui.warp_template(ui.bg_in_draw) # ui_3d_launcher need remove
-    @ui.warp_template(launcher.draw)
-    #@ui.warp_template(taskbar.mem_draw)
-    #@ui.warp_template(taskbar.battery_draw)
-    # @ui.warp_template(taskbar.mem_draw)
-    def draw_launcher():
-        ui.display()
+  def draw(self, is_check=False, alpha=0, color=(255, 255, 255)):
+    tmp = self.img.copy()  # 1ms
+    if is_check:
+      self.checked(color)
 
-    @ui.warp_template(CubeAudio.event)
-    @ui.warp_template(ui.anime_draw)
-    @ui.warp_template(CubeAudio.event)
-    #@ui.warp_template(taskbar.mem_draw)
-    # @ui.warp_template(system_info.info_draw)
-    def draw_pages():
-        if app.current != None:
-            app.current.draw()
-        ui.display()
+    old = (tmp.width(), tmp.height())
+    new = (int(alpha / 50), int(alpha / 50))
+    tmp = tmp.resize(old[0] + new[0] - 3, old[1] + new[1] - 3)
+    #print(tmp)
+    #if self.last != new[0]:
+        ##print(old, new)
+        #tmp = tmp.resize(old[0] + new[0] - 8, old[1] + new[1] - 8)
+        #self.last = new[0]
+    #print(tmp)
+    ui.canvas.draw_image(tmp,
+        self.x - int(new[0] / 2),
+        self.y - int(new[1] / 2),
+        alpha=int(alpha))  # 4ms
+    del tmp
 
-    @ui.warp_template(CubeAudio.event)
-    #@ui.warp_template(taskbar.time_draw)
-    @ui.warp_template(sample_page.sample_draw)
-    @ui.warp_template(CubeAudio.event)
-    def draw_samples():
-        ui.display()
-
-    @ui.warp_template(explorer.draw)
-    def draw_explorer():
-        # if explorer.info != "":
-        #     protect.stop()
-        #     print(explorer.get_path(explorer.paths) + '/' + explorer.info)
-        #     # with open(explorer.get_path(explorer.paths) + '/' + tmp, 'rb') as target:
-        #     #     # exec(target.read(), locals())
-        #     #     exec(target.read())
-        #     execfile(explorer.get_path(explorer.paths) + '/' + explorer.info)
-        #     protect.start()
-
-        ui.display()
-
-    def draw_camera():
-        try:
-            ai_camera.ai_draw()
-            ui.display()
-        except Exception as e:
-            app.layer = 1
-            raise e
-
-    current = None
-
-    def load_application(selected):
-        if app.current != None: # clear last application
-            del app.current
-            app.current = None
-        if selected == 0:
-            pass
-
-        elif selected == 1:
-            app.current = pages()
-        elif selected == 2:
-            pass
-            #app.layer -= 1 # return last layer
-            #raise Exception("Settings Unrealized.")
-        elif selected == 3:
-            CubeAudio.load(os.getcwd() + "/res/one.wav", 100)
-            sample_page.key_init()
-            sample_page.add_sample(sample_msa301())
-            sample_page.add_sample(sample_spmod_test())
-            sample_page.add_sample(sample_shtxx())
-            sample_page.add_demo()
+  def title(self, string, color=(255, 255, 255)):
+    ui.canvas.draw_string(self.x, self.y, string)
 
 
-    def exec_application():
-        if launcher.app_select == 0:
-            app.draw_camera()
-        if launcher.app_select == 1:
-            app.draw_pages()
-        if launcher.app_select == 2:
-            app.draw_explorer()
-        if launcher.app_select == 3:
-            try:
-                app.draw_samples()
-            except Exception as e:
-                app.layer -= 1
+class launcher:
 
-    rgb = 0
-    def rgb_change(rgb):
-        cube_led.r.value(rgb & 0b001)
-        cube_led.g.value(rgb & 0b010)
-        cube_led.b.value(rgb & 0b100)
+  alpha = 0
+  app_select = 0
+  app_sets = [
+      icon(60, 200, os.getcwd() + "/res/icons/app_camera.bmp"),
+      icon(160, 200, os.getcwd() + "/res/icons/app_settings.bmp"),
+      icon(260, 200, os.getcwd() + "/res/icons/app_explorer.bmp"),
+      icon(360, 200, os.getcwd() + "/res/icons/app_system_info.bmp"),
+  ]
 
-    @ui.warp_template(ui.blank_draw)
-    @ui.warp_template(ui.grey_draw)
-    @catch
-    def draw():
+  btn = sipeed_button()
+  agent = agent()
 
-        #app.btn.event()
-        app.btn.expand_event()
+  def init():
+    launcher.agent.event(150, launcher.key_event)
 
-        if app.btn.home() == 2: # click button release to 2
-            print('into', app.layer)
-            if app.layer == 1:
-                app.layer += 1
-                # launcher into application
-                app.load_application(launcher.app_select)
-            elif app.layer == 2:
-                if app.btn.interval() > 1000: # long press
-                    app.layer -= 1
-                    if launcher.app_select == 1:
-                        ui.anime = None # Clear
-                # application return launcher
-            else:
-                app.layer += 1
-                # help into launcher
+  def key_event():
+    launcher.btn.event()
 
-        if app.btn.next() == 1:
-            app.rgb = (app.rgb + 1) % 8
-            app.rgb_change(app.rgb)
-            if launcher.app_select == 1:
-                CubeAudio.load(os.getcwd() + "/res/jump.wav", 100)
+    if launcher.btn.back() == 1:
+        launcher.app_select -= 1
+    elif launcher.btn.next() == 1:
+        launcher.app_select += 1
+    elif launcher.btn.home() == 1:
+        print('start', launcher.app_select)
+        # ui.canvas.draw_string(15, 120, '(%s)' % launcher.app_sets[launcher.app_select])
+
+    launcher.app_select = launcher.app_select % len(launcher.app_sets)  # lock pos
 
 
-        if app.btn.back() == 1:
-            app.rgb = (app.rgb - 1) % 8
-            app.rgb_change(app.rgb)
-            if launcher.app_select == 1:
-                CubeAudio.load(os.getcwd() + "/res/alarm.wav", 100)
+  def draw():
+    launcher.agent.parallel_cycle()
+    ui.canvas.draw_string(203, 73, "Amigo",
+                          color=(64, 64, 64), scale=8, mono_space=0)
+    ui.canvas.draw_string(200, 70, "A",
+                        color=(0xFF, 0x30, 0x30), scale=8, mono_space=0)
+    ui.canvas.draw_string(200, 70, "  m",
+                        color=(0xFF, 0xFF, 0x30), scale=8, mono_space=0)
+    ui.canvas.draw_string(200, 70, "    i",
+                        color=(0x30, 0x30, 0xFF), scale=8, mono_space=0)
+    ui.canvas.draw_string(200, 70, "     g",
+                        color=(0x30, 0xFF, 0x30), scale=8, mono_space=0)
+    ui.canvas.draw_string(200, 70, "       o",
+                        color=(0xFF, 0x30, 0xFF), scale=8, mono_space=0)
 
+    value = math.cos(math.pi * launcher.alpha / 12) * 50 + 200
+    launcher.alpha = (launcher.alpha + 1) % 24
 
-        if app.layer == 0:
-            app.draw_load()
-        elif app.layer == 1:
-            gc.collect()
-            app.draw_launcher()
-        elif app.layer == 2:
-            app.exec_application()
+    for pos in range(0, len(launcher.app_sets)):
+        checked = (pos == launcher.app_select)
+        launcher.app_sets[pos].draw(checked, value if checked else 255)
 
-    def run():
-        ui.height, ui.weight = 240, 240
-        button_io.config(23, 20, 31)
-        cube_led.init(14, 15, 17, 32)
-        fm.register(24,fm.fpioa.I2C1_SCLK, force=True)
-        fm.register(27,fm.fpioa.I2C1_SDA, force=True)
-        if CubeAudio.check():
-            CubeAudio.ready()
-            fm.register(13,fm.fpioa.I2S0_MCLK, force=True)
-            fm.register(21,fm.fpioa.I2S0_SCLK, force=True)
-            fm.register(18,fm.fpioa.I2S0_WS, force=True)
-            fm.register(35,fm.fpioa.I2S0_IN_D0, force=True)
-            fm.register(34,fm.fpioa.I2S0_OUT_D2, force=True)
-
-        #app.ctrl.event(100, lambda *args: time.sleep(1))
-        #app.ctrl.event(10, app.btn.event)
-        app.ctrl.event(5, app.draw)
-        while True:
-            #import time
-            #last = time.ticks_ms()
-            while True:
-                try:
-                    #print((int)(1000 / (time.ticks_ms() - last)), 'fps')
-                    #last = time.ticks_ms()
-                    app.ctrl.cycle()
-                    protect.keep()
-                    #time.sleep(0.1)
-                except KeyboardInterrupt:
-                    protect.stop()
-                    raise KeyboardInterrupt()
-                except Exception as e:
-                    gc.collect()
-                    print(e)
-
-
+launcher.init()
 
 if __name__ == "__main__":
-    gc.collect()
-    print_mem_free()
-    app.run()
+  from ui_canvas import ui
+  ui.height, ui.weight = 480, 320
+  button_io.config(23, 31, 20) # amigo
+  @ui.warp_template(ui.blank_draw)
+  @ui.warp_template(ui.bg_in_draw)
+  @ui.warp_template(ui.bg_in_draw)
+  @ui.warp_template(launcher.draw)
+  def unit_test():
+    ui.display()
+  import time
+  last = time.ticks_ms()
+  while True:
+      print(time.ticks_ms() - last)
+      last = time.ticks_ms()
+      unit_test()
+      #time.sleep(0.5)
---
+++ ui/ui_amigo_launcher.py
@@ -0,0 +1,188 @@
+# This file is part of MaixUI
+# Copyright (c) sipeed.com
+#
+# Licensed under the MIT license:
+#   http://www.opensource.org/licenses/mit-license.php
+#
+
+import math, os, image
+
+try:
+    from ui_canvas import ui
+    from button import sipeed_button, button_io
+    from core import agent
+except ImportError:
+    from ui.ui_canvas import ui
+    from driver.button import sipeed_button, button_io
+    from lib.core import agent
+
+class icon:
+
+  def __init__(self, x, y, w = 64, h = 64):
+    self.x, self.y, self.w, self.h = x, y, w, h
+    self.img = image.Image(size=(self.w, self.h))
+
+  def checked(self, color=(255, 0, 0)):
+    ui.canvas.draw_rectangle(self.x - 2, self.y - 2,
+                             self.w + 4, self.h + 4, color, thickness=1)
+
+  def on_draw(self):
+    tmp = b"\x1F\x3F\x78\xF0\xE3\xC7\xCC\xCC\xCF\xCF\xC7\xE3\xF0\x78\x3F\x1F\xF8\xFC\x1E\x0F\xC7\xE3\xF3\xF3\xF3\xB3\xE3\xC7\x0F\x1E\xFC\xF8"
+
+    self.img.draw_font(0, 0, 16, 16, tmp, scale=4, color=(64,64,64))
+    self.img.draw_font(0, 0, 16, 16, tmp, scale=4, color=(255,255,255))
+
+  def on_title(self, color=(255, 255, 255)):
+    ui.canvas.draw_string(self.x, self.y + self.h + 5, self.__qualname__, scale=2)
+
+  def draw(self, is_check=False, alpha=0, color=(255, 255, 255)):
+    self.img.clear()
+
+    self.on_draw()
+
+    #ui.canvas.draw_string(self.x, self.y, str(alpha))
+    if is_check:
+      self.checked(color)
+
+    old = (self.img.width(), self.img.height())
+    new = (int(alpha / 50), int(alpha / 50))
+    tmp = self.img.resize(old[0] + new[0] - 10, old[1] + new[1] - 10)
+    ui.canvas.draw_image(tmp,
+        self.x - int(new[0] / 2) + 5,
+        self.y - int(new[1] / 2) + 5,
+        alpha=int(alpha))  # 4ms
+    del tmp
+
+    self.on_title()
+
+class Camera(icon):
+
+    tmp1 = b"\x00\x3E\x22\x7F\xFF\xC0\xC7\xCF\xDC\xD8\xDC\xCF\xC7\xC0\xFF\x7F\x00\x00\x00\xFE\xFF\x03\x9B\xC3\xE3\x63\xE3\xC3\x83\x03\xFF\xFE"
+    tmp2 = b"\x00\x00\x00\x00\x00\x00\x07\x0F\x1C\x18\x1C\x0F\x07\x00\x00\x00\x00\x00\x00\x00\x00\x00\x98\xC0\xE0\x60\xE0\xC0\x80\x00\x00\x00"
+
+    def on_draw(self):
+
+        self.img.draw_font(2, 2, 16, 16, Camera.tmp1, scale=4, color=(64,64,64))
+        self.img.draw_font(0, 0, 16, 16, Camera.tmp1, scale=4, color=(255,255,255))
+        self.img.draw_font(0, 0, 16, 16, Camera.tmp2, scale=4, color=(180, 180, 180))
+
+    def on_title(self, color=(255, 255, 255)):
+        ui.canvas.draw_string(self.x - 3, self.y + self.h + 5, self.__qualname__, scale=2, color=color)
+
+class Setting(icon):
+
+    tmp1 = b"\x00\x03\x19\x3B\x3F\x0E\x5C\x78\x78\x5C\x0E\x3F\x3B\x19\x03\x00\x00\xC0\x98\xDC\xFC\x70\x3A\x1E\x1E\x3A\x70\xFC\xDC\x98\xC0\x00"
+    tmp2 = b"\x00\x00\x00\x03\x0F\x0E\x1C\x18\x18\x1C\x0E\x0F\x03\x00\x00\x00\x00\x00\x00\xC0\xF0\x70\x38\x18\x18\x38\x70\xF0\xC0\x00\x00\x00"
+
+    def on_draw(self):
+
+        self.img.draw_font(2, 2, 16, 16, Setting.tmp1, scale=4, color=(64,64,64))
+        self.img.draw_font(0, 0, 16, 16, Setting.tmp1, scale=4, color=(0xCF, 0xCF, 0xCF))
+        self.img.draw_font(0, 0, 16, 16, Setting.tmp2, scale=4, color=(255, 255, 255))
+
+    def on_title(self, color=(255, 255, 255)):
+        ui.canvas.draw_string(self.x - 5, self.y + self.h + 5, self.__qualname__, scale=2, color=color)
+
+class Demo(icon):
+
+    tmp1 = b"\x00\x7F\x40\x40\x7F\x40\x40\x48\x50\x61\x53\x4A\x40\x40\x7F\x00\x00\xFE\x02\x02\xFE\x02\x02\x52\xCA\x86\x0A\x12\x02\x02\xFE\x00"
+    tmp2 = b"\x00\x7F\x40\x40\x7F\x40\x40\x40\x40\x40\x40\x40\x40\x40\x7F\x00\x00\xFE\x02\x02\xFE\x02\x02\x02\x02\x02\x02\x02\x02\x02\xFE\x00"
+
+    def on_draw(self):
+
+        self.img.draw_font(2, 2, 16, 16, Demo.tmp1, scale=4, color=(64,64,64))
+        self.img.draw_font(0, 0, 16, 16, Demo.tmp1, scale=4, color=(0xCF, 0xCF, 0xCF))
+        self.img.draw_font(0, 0, 16, 16, Demo.tmp2, scale=4, color=(0x40, 0xFF, 0x40))
+
+    def on_title(self, color=(255, 255, 255)):
+        ui.canvas.draw_string(self.x + 10, self.y + self.h + 5, self.__qualname__, scale=2, color=color)
+
+class Photo(icon):
+
+    tmp1 = b"\x3F\x60\xC0\x80\x80\x80\x80\x90\xB8\xEC\xC6\x83\x81\xC0\x60\x3F\xFC\x06\x3B\x6D\x45\x6D\x39\x01\x01\x41\xE1\xB1\x19\x0F\x06\xFC"
+    tmp2 = b"\x00\x00\x00\x00\x00\x00\x00\x10\x38\x6C\x46\x03\x01\x00\x00\x00\x00\x00\x38\x6D\x44\x6D\x38\x00\x00\x40\xE0\xB0\x18\x0E\x00\x00"
+    tmp3 = b"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x38\x6C\x44\x6C\x38\x00\x00\x00\x00\x00\x00\x00\x00\x00"
+
+    def on_draw(self):
+
+        self.img.draw_font(2, 2, 16, 16, Photo.tmp1, scale=4, color=(64,64,64))
+        self.img.draw_font(0, 0, 16, 16, Photo.tmp1, scale=4, color=(0,255,255))
+        self.img.draw_font(0, 0, 16, 16, Photo.tmp2, scale=4, color=(0x1F, 255, 0xa0))
+        self.img.draw_font(0, 0, 16, 16, Photo.tmp3, scale=4, color=(0xFF, 0xa0, 0xa0))
+
+    def on_title(self, color=(255, 255, 255)):
+        ui.canvas.draw_string(self.x + 5, self.y + self.h + 5, self.__qualname__, scale=2, color=color)
+
+class launcher:
+
+  alpha = 0
+  app_select = 0
+  app_sets = [
+      Camera(60, 200),
+      Setting(160, 200),
+      Demo(260, 200),
+      Photo(360, 200),
+  ]
+
+  btn = sipeed_button()
+  agent = agent()
+
+  def init():
+    launcher.agent.event(150, launcher.key_event)
+
+  def key_event():
+    launcher.btn.event()
+
+    if launcher.btn.back() == 1:
+        launcher.app_select -= 1
+    elif launcher.btn.next() == 1:
+        launcher.app_select += 1
+    elif launcher.btn.home() == 1:
+        print('start', launcher.app_select)
+        # ui.canvas.draw_string(15, 120, '(%s)' % launcher.app_sets[launcher.app_select])
+
+    launcher.app_select = launcher.app_select % len(launcher.app_sets)  # lock pos
+
+
+  def draw():
+    launcher.agent.parallel_cycle()
+    ui.canvas.draw_rectangle((0, 0, ui.height, ui.weight),
+                                     fill=True, color=(120, 120, 120))
+    ui.canvas.draw_string(203, 73, "Amigo",
+                          color=(64, 64, 64), scale=8, mono_space=0)
+    ui.canvas.draw_string(200, 70, "A",
+                        color=(0xFF, 0x40, 0x40), scale=8, mono_space=0)
+    ui.canvas.draw_string(200, 70, "  m",
+                        color=(0xFF, 0xFF, 0x40), scale=8, mono_space=0)
+    ui.canvas.draw_string(200, 70, "    i",
+                        color=(0x40, 0x40, 0xFF), scale=8, mono_space=0)
+    ui.canvas.draw_string(200, 70, "     g",
+                        color=(0x40, 0xFF, 0x40), scale=8, mono_space=0)
+    ui.canvas.draw_string(200, 70, "       o",
+                        color=(0xFF, 0x50, 0xFF), scale=8, mono_space=0)
+
+    value = math.cos(math.pi * launcher.alpha / 12) * 50 + 200
+    launcher.alpha = (launcher.alpha + 1) % 24
+
+    for pos in range(0, len(launcher.app_sets)):
+        checked = (pos == launcher.app_select)
+        launcher.app_sets[pos].draw(checked, value if checked else 255)
+
+launcher.init()
+
+if __name__ == "__main__":
+  from ui_canvas import ui
+  ui.height, ui.weight = 480, 320
+  button_io.config(23, 31, 20) # amigo
+  @ui.warp_template(ui.blank_draw)
+  @ui.warp_template(launcher.draw)
+  @ui.warp_template(ui.bg_in_draw)
+  def unit_test():
+    ui.display()
+  import time
+  last = time.ticks_ms()
+  while True:
+      print(time.ticks_ms() - last)
+      last = time.ticks_ms()
+      unit_test()
+      #time.sleep(0.5)
