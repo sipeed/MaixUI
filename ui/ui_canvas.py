e0c04ab3a14d47d59c1970850d66c8774d05d7c8
Juwan <junhuanchen@qq.com>
2020-08-21 19:13:45 +0800

    Beautiful logo

--- ui/ui_canvas.py
+++ ui/ui_canvas.py
@@ -45,10 +45,20 @@ class ui:
         #ui.bak.draw_rectangle((60,30,120,150), fill=True, color=(250, 0, 0))
         #ui.bak.draw_string(70, 40, "o", color=(255, 255, 255), scale=2)
         #ui.bak.draw_string(80, 10, "s", color=(255, 255, 255), scale=15)
-        ui.canvas.draw_circle(120, 120, int(50), fill=True,
+        ui.canvas.draw_circle(121, 111, int(50),
+                              color=(64, 64, 64), thickness=3)  # 10ms
+        ui.canvas.draw_circle(120, 110, int(50),
                               color=(250, 0, 0))  # 10ms
-        ui.canvas.draw_string(100, 88, "o", color=(255, 255, 255), scale=1)
-        ui.canvas.draw_string(103, 70, "s", color=(255, 255, 255), scale=8)
+        ui.canvas.draw_circle(120, 110, int(50), fill=True,
+                              color=(250, 0, 0))  # 10ms
+
+        sipeed = b'\x40\xA3\x47\x0F\x18\x38\x18\x0F\x07\x03\x00\x00\x00\x0F\x0F\x0F\x00\xFC\xFC\xFC\x00\x00\x00\xF0\xF8\xFC\x06\x07\x06\xFC\xF8\xF0'
+
+        ui.canvas.draw_font(88, 80, 16, 16, sipeed, scale=4, color=(64,64,64))
+
+        ui.canvas.draw_font(86, 78, 16, 16, sipeed, scale=4, color=(255,255,255))
+
+
         # ui.canvas = ui.canvas # 15ms
         #ui.canvas = ui.bak.copy() # 10ms 282kb
 
@@ -105,15 +115,16 @@ class ui:
         del ui.canvas
 
 if __name__ == "__main__":
+    # ui.height, ui.weight = 480, 320 # amigo
     #@ui.warp_template(ui.blank_draw)
     #@ui.warp_template(ui.bg_draw)
     #@ui.warp_template(ui.anime_draw)
     # 50ms
     @ui.warp_template(ui.blank_draw)
-    @ui.warp_template(ui.grey_draw)
+    #@ui.warp_template(ui.grey_draw)
     @ui.warp_template(ui.bg_in_draw)
-    @ui.warp_template(ui.anime_in_draw)
-    @ui.warp_template(ui.help_in_draw)
+    #@ui.warp_template(ui.anime_in_draw)
+    #@ui.warp_template(ui.help_in_draw)
     # 20ms
     def test_launcher_draw():
         #ui.bg_draw()
