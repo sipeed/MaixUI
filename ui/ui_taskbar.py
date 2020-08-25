a4ea33a060c5d76b1cc2b5b401b04842bf1871a3
Juwan <junhuanchen@qq.com>
2020-08-25 16:43:17 +0800

    remake battery view.

--- ui/ui_taskbar.py
+++ ui/ui_taskbar.py
@@ -16,13 +16,15 @@ except ImportError:
     from lib.core import agent
     from ui.ui_canvas import ui
 
-levels = [4.13, 4.06, 3.98, 3.92, 3.87,
-          3.82, 3.79, 3.77, 3.74, 3.68, 3.45, 3.00]
-
+#levels = [4.13, 4.06, 3.98, 3.92, 3.87,
+          #3.82, 3.79, 3.77, 3.74, 3.68, 3.45, 3.00]
 
 class taskbar:
 
-    now, info, last, img = '', '', 0, None
+    charge = b"\x00\x01\x03\x07\x0F\x1E\x3F\x7F\xFF\x00\x00\x01\x01\x03\x03\x06\xC0\x80\x80\x00\x00\x00\xFF\xFE\xFC\x78\xF0\xE0\xC0\x80\x00\x00"
+    normal = b"\x00\x00\x00\x7F\xC0\x80\x80\x80\x80\x80\x80\xC0\x7F\x00\x00\x00\x00\x00\x00\xFC\x06\x02\x03\x01\x01\x03\x02\x06\xFC\x00\x00\x00"
+
+    now, info, last, sync = '', '', 0, None
 
     power = None
     ctrl = agent()
@@ -42,29 +44,36 @@ class taskbar:
         ui.canvas.draw_string(int((ui.height) / 2) - 60, 2, "%02u:%02u:%02u" % (
             taskbar.now[3], taskbar.now[4], taskbar.now[5]), color=(255, 255, 255), scale=2, mono_space=1)
 
+    pos = 0
+
     def mem_draw():
         info = 'Rmnng %s KB' % str(gc.mem_free() / 1024)
         ui.canvas.draw_string(10, 2, info, scale=2)
 
     def battery_sync():
-        vbat_voltage = taskbar.power.getVbatVoltage() / 1000
-        taskbar.info = "{0:.2f}V".format(
-            vbat_voltage, taskbar.power.is_charging())
-        if taskbar.img != None:
-            del taskbar.img
-        pos = int((4.2 - vbat_voltage) / 0.2)
-        if pos < 6:
-            tmp = "charge" if taskbar.power.is_charging() else "normal"
-            taskbar.img = image.Image(
-                "res/icons/battery/{0} ({1}).jpg".format(tmp, 6 - pos))
+        try:
+            vbat_voltage = taskbar.power.getVbatVoltage() / 1000
+            vbat_value = int((vbat_voltage - 3.12) * 100)
+            taskbar.sync = (taskbar.power.is_charging(), vbat_value)
+        except Exception as e:
+            gc.collect()
+            print(e)
 
     def battery_draw():
         taskbar.ctrl.cycle()
-        if taskbar.img != None:
+        if taskbar.sync != None:
             #ui.canvas.draw_rectangle((0,0,240,25), fill=True, color=(50, 50, 50))
-            ui.canvas.draw_image(taskbar.img, ui.height - 32, 2, alpha=int(255))
-        ui.canvas.draw_string(10, 1, taskbar.info,
-                              scale=2, color=(50, 255, 50))
+            #ui.canvas.draw_image(taskbar.img, ui.height - 32, 2, alpha=int(255))
+            charge, value = taskbar.sync
+            ui.canvas.draw_font(ui.height - 40, 0, 16, 16, taskbar.normal, scale=2, color=(300 - value * 3, value * 2, 50))
+            if charge == True:
+                ui.canvas.draw_font(ui.height - 60, 8, 16, 16, taskbar.charge, scale=1, color=(300 - value * 3, value * 2, 50))
+
+            #print('{0:>2d}%'.format(value))
+            ui.canvas.draw_string(ui.height - 32, 10, '{0:>2d}%'.format(value), scale=1, color=(255,255,255))
+
+        #ui.canvas.draw_string(10, 1, taskbar.info,
+                              #scale=2, color=(50, 255, 50))
 
 #taskbar.init()
 
@@ -90,12 +99,12 @@ if __name__ == "__main__":
 
     ui.height, ui.weight = 480, 320
 
-    def tmp_draw():
-        ui.canvas.draw_rectangle(
-            (0, 0, 240, 240), fill=True, color=(75, 75, 75))
+    #def tmp_draw():
+        #ui.canvas.draw_rectangle(
+            #(0, 0, 240, 240), fill=True, color=(75, 75, 75))
 
     @ui.warp_template(ui.blank_draw)
-    @ui.warp_template(tmp_draw)
+    #@ui.warp_template(tmp_draw)
     @ui.warp_template(ui.anime_in_draw)
     @ui.warp_template(taskbar.time_draw)
     @ui.warp_template(taskbar.battery_draw)
@@ -104,8 +113,12 @@ if __name__ == "__main__":
     import time
     last = time.ticks_ms()
     while True:
+        #print(time.ticks_ms() - last)
+        last = time.ticks_ms()
+        app_main()
+        continue
         try:
-            print(time.ticks_ms() - last)
+            #print(time.ticks_ms() - last)
             last = time.ticks_ms()
             app_main()
         except Exception as e:
