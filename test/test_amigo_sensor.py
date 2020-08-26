d6baf4ee915156b01adfab206c8934328d25bbf7
Juwan <junhuanchen@qq.com>
2020-08-26 15:48:11 +0800

    Update test_amigo_sensor.py

--- test/test_amigo_sensor.py
+++ test/test_amigo_sensor.py
@@ -1,15 +1,6 @@
-# Hello World Example
-#
-# Welcome to the MaixPy IDE!
-# 1. Conenct board to computer
-# 2. Select board at the top of MaixPy IDE: `tools->Select Board`
-# 3. Click the connect buttion below to connect board
-# 4. Click on the green run arrow button below to run the script!
 
 import sensor, image, time, lcd
-# -*- coding: UTF-8 -*-
-# Untitled - By: Echo - 周五 4月 2 2020
-# start of pmu_axp173.py
+
 from machine import I2C, Timer
 import lcd, utime
 from fpioa_manager import fm
@@ -37,9 +28,6 @@ class AXP173:
             self.axp173Addr, reg_address, value, mem_size=8)
     def writeREG(self, regaddr, value):
         self.__write_reg(regaddr, value)
-# end of pmu_axp173.py
-
-# ------------------------
 
 lcd.init()
 i2cDev = I2C(I2C.I2C0, freq=400000, scl=24, sda=27)
@@ -50,23 +38,6 @@ axp173.writeREG(0x27, 0x20)
 axp173.writeREG(0x28, 0x0C)
 
 lcd.init(freq=20000000)
-
-#sensor.reset(choice=2)                      # Reset and initialize the sensor. It will
-                                    ## run automatically, call sensor.run(0) to stop
-#sensor.set_pixformat(sensor.YUV422) # Set pixel format to RGB565 (or GRAYSCALE)
-#sensor.set_framesize(sensor.QVGA)   # Set frame size to QVGA (320x240)
-##sensor.sleep(1)
-#sensor.skip_frames(time = 5000)     # Wait for settings take effect.
-#clock = time.clock()                # Create a clock object to track the FPS.
-
-#while(True):
-    #clock.tick()                    # Update the FPS clock.
-    #img = sensor.snapshot()         # Take a picture and return the image.
-    #lcd.display(img)                # Display on LCD
-    ## print(clock.fps())              # Note: MaixPy's Cam runs about half as fast when connected
-                                    ## to the IDE. The FPS should increase once disconnected.
-
-
 
 while True:
 
