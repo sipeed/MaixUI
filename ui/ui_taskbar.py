461df64ebffb692502f5003af173f0e3d07dba17
Juwan <junhuanchen@qq.com>
2020-08-26 14:04:25 +0800

    ui_taskbar.py lock vbat_value < 100

--- ui/ui_taskbar.py
+++ ui/ui_taskbar.py
@@ -54,6 +54,8 @@ class taskbar:
         try:
             vbat_voltage = taskbar.power.getVbatVoltage() / 1000
             vbat_value = int((vbat_voltage - 3.2) * 100)
+            if vbat_value > 100:
+                vbat_value = 100
             taskbar.sync = (taskbar.power.is_charging(), vbat_value)
         except Exception as e:
             gc.collect()
