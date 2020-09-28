0a4f454eb2345040c0bdefcf5a4a8daeda07fe1b
Juwan <junhuanchen@qq.com>
2020-09-28 18:23:26 +0800

    [agent] event allow args.

--- lib/core.py
+++ lib/core.py
@@ -17,9 +17,9 @@ class agent:
     # return time.time() * 1000
     return time.ticks_ms()
 
-  def event(self, interval, function):
-    # arrived, interval, function
-    tmp = (self.get_ms() + interval, interval, function)
+  def event(self, cycle, func, args=None):
+    # arrived, cycle, function
+    tmp = (self.get_ms() + cycle, cycle, func, args)
     #print('self.event', tmp)
     self.msg.append(tmp)
 
@@ -32,22 +32,23 @@ class agent:
           break
     #print(self.msg)
 
+  def call(self, obj, pos=0):
+    #print(self.call, pos, obj)
+    self.msg.pop(pos)
+    self.event(obj[1], obj[2], obj[3])
+    obj[2](obj[3]) if obj[3] else obj[2]() # exec function
+
   def cycle(self):
     if (len(self.msg)):
-      tmp = self.msg[0]
-      if (self.get_ms() >= tmp[0]):
-        self.msg.pop(0)
-        self.event(tmp[1], tmp[2])
-        tmp[2](self)  # function
+      obj = self.msg[0]
+      if (self.get_ms() >= obj[0]):
+        self.call(obj, 0)
 
   def parallel_cycle(self):
     for pos in range(len(self.msg)): # maybe use map
-      tmp = self.msg[pos]
-      if (self.get_ms() >= tmp[0]):
-        #print('self.parallel_cycle', pos, tmp)
-        self.msg.pop(pos)
-        self.event(tmp[1], tmp[2])
-        tmp[2](self)  # function
+      obj = self.msg[pos]
+      if (self.get_ms() >= obj[0]):
+        self.call(obj, pos)
         break
 
   def unit_test(self):
@@ -56,7 +57,7 @@ class agent:
         def test_0(self):
           print('test_0')
 
-        def test_1(self):
+        def test_1():
           print('test_1')
 
         def test_2(self):
@@ -66,9 +67,9 @@ class agent:
           self.remove(tmp.test_2)
 
     import time
-    self.event(200, tmp.test_0)
+    self.event(200, tmp.test_0, self)
     self.event(10, tmp.test_1)
-    self.event(2000, tmp.test_2)
+    self.event(2000, tmp.test_2, self)
     while True:
       self.parallel_cycle()
       time.sleep(0.1)
